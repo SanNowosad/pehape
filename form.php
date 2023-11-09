cc0362520bde87cdf32d22cf11926409ed14a923
SanNowosad <144989746+SanNowosad@users.noreply.github.com>
2023-11-09 08:42:10 +0100

    Dodanie plików

--- form.php
+++ form.php
@@ -3,25 +3,27 @@
 
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
 
-    $email = htmlspecialchars($_POST['email']);
-    $nick = htmlspecialchars($_POST['nick']);
-    $password = htmlspecialchars($_POST['password']);
+    $noemail = "";
+    $nonick = "";
+    $nopassword = "";
 
-    if(empty($email)){
-        $noemail = 1;
-    }
-    else if(empty($nick) or empty($password)){
-        $nonick = 1;
-    }
-    else if(empty($password)){
-        $nopassword = 1;
-    }
-    else {
-        $noemail = 0;
-        $nonick = 0;
-        $nopassword = 0;
-    }
+    // if(isset($_POST['submit'])){
+        $email = htmlspecialchars($_POST['email']);
+        $nick = htmlspecialchars($_POST['nick']);
+        $password = htmlspecialchars($_POST['password']);
 
+        if(empty($email)){
+            $noemail = "type in your email";
+        }
+        if(empty($nick) or empty($password)){
+            $nonick = "type in your nickname";
+        }
+        if(empty($password)){
+            $nopassword = "type in your password";
+        }
+        echo "email $email; nick $nick; pass $password";
+        header("Location: signup.php");
+    // }
 }
 else {
     header("Location: index.html");
--- signup.php
+++ signup.php
@@ -1,25 +1,3 @@
-<?php
-    $noemail = "";
-    $nonick = "";
-    $nopassword = "";
-
-    if(isset($_POST['submit'])){
-        $email = htmlspecialchars($_POST['email']);
-        $nick = htmlspecialchars($_POST['nick']);
-        $password = htmlspecialchars($_POST['password']);
-
-        if(empty($email)){
-            $noemail = "type in your email";
-        }
-        if(empty($nick) or empty($password)){
-            $nonick = "type in your nickname";
-        }
-        if(empty($password)){
-            $nopassword = "type in your password";
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,8 +11,8 @@
         <div class="form__container">
             <a href="/pehap/ufo/" class="form__logo">KTopper</a>
             <h1>sign up</h1>
-            <!-- <form action="form.php" method="post"> -->
-            <form method="post">
+            <form action="form.php" method="post">
+            <!-- <form action="?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post"> -->
                 <label for="email">e-mail</label>
                 <input type="email" name="email" id="email" placeholder="your e-mail address">
                 <span><?php echo $noemail ?></span>
