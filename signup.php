c33fc605e1a20f113ddba1ac0c9f936b5d4141b2
San Nowosad <San Nowosad>
2023-11-17 08:35:11 +0100

    change (forms)

--- signup.php
+++ signup.php
@@ -13,12 +13,12 @@
             <h1>sign up</h1>
             <form action="signup.php" method="get">
                 <label for="email">e-mail</label>
-                <input type="email" name="email" id="email" placeholder="your e-mail address">
+                <input type="email" name="email" id="email" placeholder="your e-mail address" value="<?php echo $_GET['email'];?>">
                 <span><?php 
                     if ($_GET['email']==""){
                         echo "No email";} ?></span>
                 <label for="nick">nickname</label>
-                <input type="text" name="nick" id="nick" placeholder="your nickname">
+                <input type="text" name="nick" id="nick" placeholder="your nickname" value="<?php echo $_GET['nick'];?>">
                 <span><?php if ($_GET['nick']==""){
                         echo "No nickname";} ?></span>
                 <label for="password">password</label>
@@ -26,7 +26,7 @@
                 <span><?php if ($_GET['password']==""){
                         echo "No password";} ?></span>
                 <br>
-                <input type="submit" value="sign me up">
+                <input type="submit" name="submit" value="sign me up">
             </form>
         </div>
     </div>
