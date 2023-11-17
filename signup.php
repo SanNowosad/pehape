62c0d1f0b22d2c6d65e8d0907f259b7fdb6d1e9c
San Nowosad <San Nowosad>
2023-11-17 08:32:58 +0100

    Poprawki + form

--- signup.php
+++ signup.php
@@ -1,25 +1,3 @@
-<?php
-    $noemail = "";
-    $nonick = "";
-    $nopassword = "";
-
-    if(isset($_POST['submit'])){
-        $email = htmlspecialchars($_POST['email']);
-        $nick = htmlspecialchars($_POST['nick']);
-        $password = htmlspecialchars($_POST['password']);
-
-        if(empty($email)){
-            $noemail = "type in your email";
-        }
-        if(empty($nick) or empty($password)){
-            $nonick = "type in your nickname";
-        }
-        if(empty($password)){
-            $nopassword = "type in your password";
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,21 +11,22 @@
         <div class="form__container">
             <a href="/pehap/ufo/" class="form__logo">KTopper</a>
             <h1>sign up</h1>
-            <!-- <form action="form.php" method="post"> -->
-            <form method="post">
+            <form action="signup.php" method="get">
                 <label for="email">e-mail</label>
                 <input type="email" name="email" id="email" placeholder="your e-mail address">
-                <span><?php echo $noemail ?></span>
+                <span><?php 
+                    if ($_GET['email']==""){
+                        echo "No email";} ?></span>
                 <label for="nick">nickname</label>
                 <input type="text" name="nick" id="nick" placeholder="your nickname">
-                <span><?php echo $nonick ?></span>
+                <span><?php if ($_GET['nick']==""){
+                        echo "No nickname";} ?></span>
                 <label for="password">password</label>
                 <input type="password" name="password" id="password" placeholder="your secret password">
-                <span><?php echo $nopassword ?></span>
-                <!-- <input type="checkbox" value="I have read Privacy Policy of KTopper and agree to all of it."> -->
+                <span><?php if ($_GET['password']==""){
+                        echo "No password";} ?></span>
                 <br>
                 <input type="submit" value="sign me up">
-                <!-- <button class="form__container--btn"><a href="/pehap/ufo/">Sign Up</a></button> -->
             </form>
         </div>
     </div>
