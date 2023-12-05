374299ce373ae7ef55b03e02f44582e188f04f54
San Nowosad <San Nowosad>
2023-12-05 22:04:25 +0100

    secret

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@
             echo "<h2>hi ".$nick."<br/> here you can see all your data we hold</h2>";
             echo "<p>your nick is ".$nick."</p>";
             echo "<p>your email is ".$email."</p>";
-            echo "<p>And your nick is... a secrt!</p>";
+            echo "<p>And your nick is... a secret!</p>";
         ?>
     </div>
 </body>
